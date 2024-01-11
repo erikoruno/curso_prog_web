e9f535b961db32d3cfc5c47ccc95401b87fd6866
Erick <202100366a@utea.edu.pe>
2024-01-11 09:47:10 -0500

    agregado css

--- ejemplo2.php
+++ ejemplo2.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - HTML5</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
@@ -12,7 +13,14 @@
         <h3>Fundamento de programación Web</h3>
     </header>
     <section>
-        
+        <?php
+            echo "<br>1. Integrar código PHP en HTML5";
+            echo "<br>2. pagina estatica versus Página Dinámica";
+            echo "<br>3. Manejo de variables PHP";
+            echo "<br>4. Aprender el lenguaje PHP";
+            echo "<br>5. Lograr hacer formularios";
+            echo "<br>6. Paginas web";
+        ?>    
     </section>
 
     <footer>
