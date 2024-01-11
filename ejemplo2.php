3ec0bcc4c29420065f801214ff5f349dfda9b3cc
Erick <202100366a@utea.edu.pe>
2024-01-11 10:00:15 -0500

    printf

--- ejemplo2.php
+++ ejemplo2.php
@@ -14,12 +14,21 @@
     </header>
     <section>
         <?php
+        // echo"";
+        // echo '';
             echo "<br>1. Integrar código PHP en HTML5";
             echo "<br>2. pagina estatica versus Página Dinámica";
             echo "<br>3. Manejo de variables PHP";
             echo "<br>4. Aprender el lenguaje PHP";
             echo "<br>5. Lograr hacer formularios";
             echo "<br>6. Paginas web";
+        // printf("");
+        printf("<br> El sueldo básico es:");
+        printf("<h3> El sueldo básico es: </h3>");
+
+        $sueldoBasico = 350;
+        printf("El sueldo básico es: %.3f", $sueldoBasico );
+
         ?>    
     </section>
 
