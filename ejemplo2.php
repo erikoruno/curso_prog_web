0271cdd29664db3a695607995b0f8240a873a45c
Erick <202100366a@utea.edu.pe>
2024-01-11 09:06:10 -0500

    Primer commit

---
+++ ejemplo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - HTML5</title>
+</head>
+
+<body>
+
+    <header>
+        <h3>Fundamento de programación Web</h3>
+    </header>
+    <section>
+        
+    </section>
+
+    <footer>
+    <h6>Derechos reservados - </h6>
+    </footer>
+</body>
+</html>
