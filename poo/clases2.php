c4d6257df1b8af59b444d04462bbf4bfdd4d4847
Erick <202100366a@utea.edu.pe>
2024-02-01 09:06:28 -0500

    clase autos

--- poo/clases2.php
+++ poo/clases2.php
@@ -4,8 +4,7 @@ class Automovil{
     
         public $placa;
         public $marca;
-                public $modelo;
-        
+        public $modelo;
         public $año;
         public $color;
 }
@@ -13,4 +12,43 @@ class Automovil{
         $auto1 = new Automovil;
         $auto2 = new Automovil;
         $auto3 = new Automovil;
+
+        //objeto auto1
+        $auto1->placa = "ADN714";
+        $auto1->marca = "Hyundai";
+        $auto1->modelo = "Tucson";
+        $auto1->año = 2022;
+        $auto1->color = "azul";
+
+        //objeto auto2
+
+        $auto2->placa = "X4V840";
+        $auto2->marca = "Toyota";
+        $auto2->modelo = "Hilux";
+        $auto2->año = 2020;
+        $auto2->color = "Plomo";
+
+        //objeto auto3
+        $auto3->placa = "PQB856";
+        $auto3->marca = "Subaru";
+        $auto3->modelo = "Inresa";
+        $auto3->año = 2023;
+        $auto3->color = "Blanco";
+
+        //objeto auto4
+        $auto4->placa = "grt567";
+        $auto4->marca = "Ford";
+        $auto4->modelo = "Frontier";
+        $auto4->año = 2021;
+        $auto4->color = "Negro";
+
+        //objeto auto5
+        $auto5->placa = "qwe456";
+        $auto5->marca = "Kia";
+        $auto5->modelo = "Rio";
+        $auto5->año = 2024;
+        $auto5->color = "Rojo";
+
+        
+
 ?>
